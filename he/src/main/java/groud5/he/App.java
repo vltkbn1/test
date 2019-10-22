b9fc5c09f8c107af8a0d44c053296021b10c8750
quocviet <vietbn1200@gmail.com>
2019-10-23 01:07:33 +0700

    vit

---
+++ he/src/main/java/groud5/he/App.java
@@ -0,0 +1,13 @@
+package groud5.he;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
